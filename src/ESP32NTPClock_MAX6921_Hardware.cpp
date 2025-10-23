7bfbb93d51a145dc924bbe8fcee8a8bb10a648b7
v01dma1n <irek@rybark.com>
2025-10-22 20:34:07 -0400

    gridMap moved to .ino out of the driver

--- src/ESP32NTPClock_MAX6921_Hardware.cpp
+++ src/ESP32NTPClock_MAX6921_Hardware.cpp
@@ -1,22 +1,9 @@
 #include "ESP32NTPClock_MAX6921_Hardware.h"
 
-// Grids (Digits)
-static const unsigned long GRIDS[] = {
-    0b00000000010000000000, // GRD_01
-    0b00000000100000000000, // GRD_02
-    0b00000010000000000000, // GRD_03
-    0b00010000000000000000, // GRD_04
-    0b10000000000000000000, // GRD_05
-    0b00000000000000000010, // GRD_06
-    0b00000000000000000100, // GRD_07
-    0b00000000000000100000, // GRD_08
-    0b00000000000100000000, // GRD_09
-    0b00000000001000000000  // GRD_10
-};
-
-
-DispDriverMAX6921_Hardware::DispDriverMAX6921_Hardware(int sclkPin, int misoPin, int mosiPin, int ssPin, int blankPin)
-    : _blankPin(blankPin), _ssPin(ssPin) {
+DispDriverMAX6921_Hardware::DispDriverMAX6921_Hardware(int sclkPin, int misoPin, int mosiPin, int ssPin, int blankPin,
+    const unsigned long* gridMap, int digitCount)
+    : _blankPin(blankPin), _ssPin(ssPin),
+      _gridMap(gridMap), _digitCount(digitCount) {
     // Use VSPI
     _spi = new SPIClass(VSPI);
     _spi->begin(sclkPin, misoPin, mosiPin, ssPin); 
@@ -27,7 +14,7 @@ DispDriverMAX6921_Hardware::~DispDriverMAX6921_Hardware() {
         _spi->end();
         delete _spi;
     }
-}
+} 
 
 void DispDriverMAX6921_Hardware::begin() {
     pinMode(_blankPin, OUTPUT);
@@ -37,7 +24,7 @@ void DispDriverMAX6921_Hardware::begin() {
 }
 
 void DispDriverMAX6921_Hardware::spiCmd(unsigned long data) {
-    _spi->beginTransaction(SPISettings(MAX6921_SPI_SPEEDMODE0));
+    _spi->beginTransaction(SPISettings(MAX6921_SPI_SPEED, MSBFIRST, SPI_MODE0));
     digitalWrite(_ssPin, LOW);
     _spi->transfer32(data); 
     digitalWrite(_ssPin, HIGH);
@@ -45,9 +32,9 @@ void DispDriverMAX6921_Hardware::spiCmd(unsigned long data) {
 }
 
 void DispDriverMAX6921_Hardware::writeDigit(int digit, unsigned long segments) {
-    if (digit < 0 || digit >= MAX6921_DIGIT_COUNT clause
+    if (digit < 0 || digit >= _digitCount) return; 
 
     digitalWrite(_blankPin, HIGH); // Blank display
-    spiCmd(GRIDS[digit] | segments); // Send data
+    spiCmd(_gridMap[digit] | segments); // Send data
     digitalWrite(_blankPin, LOW);  // Unblank display
 }
--- src/ESP32NTPClock_MAX6921_Hardware.h
+++ src/ESP32NTPClock_MAX6921_Hardware.h
@@ -9,7 +9,8 @@
 
 class DispDriverMAX6921_Hardware {
 public:
-    DispDriverMAX6921_Hardware(int sclkPin, int misoPin, int mosiPin, int ssPin, int blankPin);
+    DispDriverMAX6921_Hardware(int sclkPin, int misoPin, int mosiPin, int ssPin, int blankPin,
+                               const unsigned long* gridMap, int digitCount);
     ~DispDriverMAX6921_Hardware(); 
 
     void begin();
@@ -21,6 +22,9 @@ private:
     int _blankPin;
     int _ssPin;
     SPIClass* _spi;
+
+    const unsigned long* _gridMap; // To store the pointer to the VFD_GRIDS array
+    int _digitCount;             // To store the VFD_DIGIT_COUNT
 };
 
 #endif // ESP32NTPCLOCK_MAX6921_HARDWARE_H
